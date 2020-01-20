ff3abfb582c7fc9bcd46c6532f16d921dfc7493a
Sagar Salian <sagar@quolam.com>
2020-01-20 15:18:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'any-ecommerce-website-url'
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content,'html.parser')
+
+product_names = []
+products = soup.find_all('a', class_='class_name')
+
+for product in products:
+	product_names.append(product.find('div', class_='_3wU53n').text)
+
+data = pd.DataFrame({'Product Name': product_names})
+data.to_csv('products.csv', index=False, encoding='utf-8')
+
+	# print(product, end='\n'*2)
